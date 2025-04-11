da0b53e178a4bbd0877bc16a3bdeb6b0d9b68e69
Annchella <annchella180@gmail.com.>
2025-04-11 21:44:29 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const Home= () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog Dashboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
